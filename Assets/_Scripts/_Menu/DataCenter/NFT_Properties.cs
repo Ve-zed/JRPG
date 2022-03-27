1f1bc35bc167cd9b38464a6ada5e9eea2b496d91
Remslt <masselotremy@gmail.com>
2022-03-27 22:37:08 +0200

    Modif Arbre NFT 1.3

--- Assets/_Scripts/_Menu/DataCenter/NFT_Properties.cs
+++ Assets/_Scripts/_Menu/DataCenter/NFT_Properties.cs
@@ -20,27 +20,36 @@ public class NFT_Properties : MonoBehaviour
     [SerializeField] TextMeshProUGUI _nFTModifTextName;
     [SerializeField] TextMeshProUGUI _nFTModifTextDesc;
 
-    //NFT count
-    [SerializeField] GameObject _nFTCountTrading;
-
-    //Nombre
+    //NFT count and trading
     public List<GameObject> NFTCount;
-
-    private bool Selected = false;
+    public List<GameObject> NFTTrading;
 
     private void Update()
     {
-        if (Selected == true)
+        if (Input.GetMouseButtonDown(0))
         {
-            _nFTSprite.sprite = _nFTSpriteSelected;
-            _nFTModifTextName.text = _nFTTextName.text;
-            _nFTModifTextDesc.text = _nFTTextDesc.text;
-            _nFTCountTrading.SetActive(true);
+            Vector3 world = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D Click = Physics2D.Raycast(world, Vector2.zero);
+            
+            if (Click.collider != null)
+            {
+                _nFTSprite.sprite = _nFTSpriteSelected;
+                _nFTModifTextName.text = _nFTTextName.text;
+                _nFTModifTextDesc.text = _nFTTextDesc.text;
+                for (int i = 0; i < NFTTrading.Count; i++)
+                {
+                    NFTTrading[i].SetActive(true);
+                }
+            }
+            /*else
+            {
+                _nFTSprite.sprite = _nFTSpriteUnlock;
+                for (int i = 0; i < NFTTrading.Count; i++)
+                {
+                    NFTTrading[i].SetActive(false);
+                }
+            }*/
         }
-    }
 
-    public void OnClickNFT()
-    {
-        Selected = true;
     }
 }
--- Assets/_Scripts/_Menu/DataCenter/NFT_Selected.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NFT_Selected : MonoBehaviour
-{
-    
-}
